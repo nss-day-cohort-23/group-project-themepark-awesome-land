96ff83a5578f9de3b809602c5c7a22f6ecabe6bb
Spencer York <spenceryork@Bernies-MacBook-Pro.local>
2018-01-13 07:58:13 -0600

    events not showing when park is closed

--- dist/bundle.js
+++ dist/bundle.js
@@ -38,40 +38,43 @@ const formatter = require('./formatter');
 
 // factory.getAttractions();
 
-factory.getAttractions().then(data => {
-    // let currentArr = [];
-    let currentTime = new Date();
-    let thisHour = currentTime.getHours();
-    if (thisHour > 12) {
-        thisHour = thisHour - 12;
-    }
-    if (thisHour === 1) {
-        thisHour = thisHour + ":";
-    }
-    let stringHour = thisHour.toString();
-    // let promiseArr = [];
-    data.forEach( attraction => {
-        if (attraction.times) {
-        let attractionTimeLength = attraction.times;
-            for(let i=0; i < attractionTimeLength.length; i++) {
-                if(attraction.times[i].startsWith(stringHour)) {
-                    let attracId = attraction.area_id;
-                    // console.log("formatter.getAreaName(attracId) with then", formatter.getAreaName(attracId).then( areaName => areaName));
-                    // call a function whose job it is to call firebase for that specific attractions area. attraction.time.length[i]
-                    $("#sidebarContent").append(`<li class="attractionName">${attraction.name} <span>(${formatter.getAreaName(attracId).then( areaName => areaName)})</span></li>`);
-                }
-            }
-            // return Promise.all(promiseArr).then( data => {
-            // console.log("promise all", data);
-            // });
+let currentTime = new Date();
+let thisHour = currentTime.getHours();
+console.log("thishour", thisHour);
+
+if (thisHour >= 10 && thisHour < 22) {
+    factory.getAttractions().then(data => {
+        if (thisHour > 12 && thisHour < 22) {
+            thisHour = thisHour - 12;
         }
+        if (thisHour === 1) {
+            thisHour = thisHour + ":";
+        }
+        let stringHour = thisHour.toString();
+        // let promiseArr = [];
+        data.forEach( attraction => {
+            if (attraction.times) {
+            let attractionTimeLength = attraction.times;
+                for(let i=0; i < attractionTimeLength.length; i++) {
+                    if(attraction.times[i].startsWith(stringHour)) {
+                        let attracId = attraction.area_id;
+                        // console.log("formatter.getAreaName(attracId) with then", formatter.getAreaName(attracId).then( areaName => areaName));
+                        $("#sidebarContent").append(`<li class="attractionName">${attraction.name} (${formatter.getAreaName(attracId).then( areaName => areaName)})</li>`);
+                    }
+                }
+                // return Promise.all(promiseArr).then( data => {
+                // console.log("promise all", data);
+                // });
+            }
+        });
     });
-});
+} else {
+    $("#sidebarContent").html("Park is currently closed.");
+}
+
 
 // formatter.getAreaName();
 
-
-// in new function pull out the area for that function. When the object comes back grab the name and add it to the object
 
 
 
