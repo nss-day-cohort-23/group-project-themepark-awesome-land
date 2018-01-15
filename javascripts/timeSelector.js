dc7cb19d32d6a6ea548dff22dd254105e0fd9e2a
BrandonAllenGreen <green04@gmail.com>
2018-01-14 22:55:15 -0600

    updated dropdown selector JS

---
+++ javascripts/timeSelector.js
@@ -0,0 +1,69 @@
+"use strict";
+const $ = require('jquery');
+const dataFactory = require('./factory');
+
+const $time = $('.dropdown-item');
+const $dropMenu = $('.dropdown-menu');
+
+// arrays to hold sorted attractions
+let withTimes = [];
+let withoutTimes = [];
+
+// event listener for dropdown items
+$time.on("click", (e) => {
+  let clickedTime = event.target.innerText;
+// load data and sort into 2 arrays based on if attraction has starting times or not
+  dataFactory.getAll()
+    .then((data) => {
+      let attractions = data.attractions;
+      let areas = data.areas;
+      attractions.forEach((attraction) => {
+        areas.forEach((area) => {
+          if (attraction.area_id === area.id) {
+            attraction.area = area.name;
+          }
+        });
+      });
+      for (let i = 0; i < attractions.length; i++) {
+        if (attractions[i].times !== undefined) {
+          withTimes.push(attractions[i]);
+        } else {
+          withoutTimes.push(attractions[i]);
+        }
+      }
+    //loop thru times 
+      for(let j = 0; j > withTimes.length; j++) {
+        console.log(withTimes[j].times);
+      }
+      
+    console.log("has times", withTimes, "doesn't have times", withoutTimes);
+    });
+  });
+       
+           
+          
+       
+
+      
+  
+
+
+
+
+
+  
+  
+  //   dataFactory.getAttractions()
+  //     .then((data) => {
+    //       for (let i = 0; i < data.length; i++) {
+      //         if (data[i].times !== undefined) {
+        //           attractionsWithTimes.push(data[i]);
+        
+        
+        //         } else {
+          //           withoutTimes.push(data[i]);
+          //         }
+          //       }
+          //     });
+          //   //console.log("Has times", attractionsWithTimes, "Doesn't have times", withoutTimes);
+          // });
